e3b00daad6d77fafa037635c0a7acb973199c875
Long Lin <92683777+LongLinKU@users.noreply.github.com>
2024-03-01 13:15:46 +0100

    Update Basic_functions.R
    getopt() and print_log()

--- Basic_functions.R
+++ Basic_functions.R
@@ -24,3 +24,25 @@ signif_with_tailing_zeros <- function(number,digits,...){
 		rounded
 	}
 }
+
+
+########### in and out for R script
+#### input using getopt()
+# library(getopt)
+
+# spec <-matrix(c(
+#     "first",'f',2,"integer","This is first",		     #short name for the flag can be only length 1, which means limitation of number of parameters. 
+#     "second",'s',1,"character",'this is second',  	     # 1 means 1 parameter value must be given after the flag. 
+#     "third","t",2,'double','this is third',		     # 2 menas parameter value is not necessarily needed.
+#     "four","o",0,'','this if four'                         # 0 means no parameter value but only the flag. Type can only be logical even you assign it numeric. No need to write it. 
+# ),byrow=T,ncol=5)
+
+## output log like plink
+print_log <- function(logfile=''){
+    args = commandArgs(trailingOnly = F)
+    args[4] =  strsplit(args[4],'=' )[[1]][2]
+    if(logfile == ''){
+        logfile = paste0(args[4],'.log')    
+    }
+    cat(args[c(1,4,6:length(args))],file=logfile)
+}
